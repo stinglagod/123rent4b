ec0eb1f1ac00c6c8e7ceafccc07a1c9c0f34b028
Алексей Яковлев(dell.home) <busenov@ya.ru>
2020-01-15 21:37:48 +0300

    Убрал фронтенд из ветки

--- frontend/config/main.php
+++ frontend/config/main.php
@@ -1,5 +1,4 @@
 <?php
-use \kartik\datecontrol\Module;
 $params = array_merge(
     require __DIR__ . '/../../common/config/params.php',
     require __DIR__ . '/../../common/config/params-local.php',
@@ -14,62 +13,8 @@ return [
     'homeUrl' => '/',
     'bootstrap' => ['log'],
     'controllerNamespace' => 'frontend\controllers',
-    'defaultRoute' => 'catalog/index',
+    'defaultRoute' => 'site/index',
     'language'=>'ru',
-    'modules' => [
-        'datecontrol' =>  [
-            'class' => 'kartik\datecontrol\Module',
-
-            // format settings for displaying each date attribute (ICU format example)
-            'displaySettings' => [
-//                Module::FORMAT_DATE => 'dd.MM.yyyy',
-                Module::FORMAT_DATE => 'dd.MM.yyyy',
-                Module::FORMAT_TIME => 'hh:mm:ss a',
-                Module::FORMAT_DATETIME => 'dd.MM.yyyy hh:mm:ss a',
-            ],
-
-            // format settings for saving each date attribute (PHP format example)
-            'saveSettings' => [
-                Module::FORMAT_DATE => 'php:Y-m-d H:i:s',
-                Module::FORMAT_TIME => 'php:H:i:s',
-                Module::FORMAT_DATETIME => 'php:Y-m-d H:i:s',
-            ],
-
-//            // set your display timezone
-//            'displayTimezone' => 'Europa/Moscow',
-//
-//            // set your timezone for date saved to db
-//            'saveTimezone' => 'UTC',
-
-            // automatically use kartik\widgets for each of the above formats
-            'autoWidget' => true,
-            // use ajax conversion for processing dates from display format to save format.
-            'ajaxConversion' => false,
-
-            // default settings for each widget from kartik\widgets used when autoWidget is true
-            'autoWidgetSettings' => [
-                kartik\datecontrol\Module::FORMAT_DATE => ['type'=>2, 'pluginOptions'=>[
-                    'autoclose'=>true,
-                    'todayHighlight' => true,
-                    'todayBtn' => true,
-                ]], // example
-                Module::FORMAT_DATETIME => [], // setup if needed
-                Module::FORMAT_TIME => [], // setup if needed
-            ],
-
-            // custom widget settings that will be used to render the date input instead of kartik\widgets,
-            // this will be used when autoWidget is set to false at module or widget level.
-            'widgetSettings' => [
-                Module::FORMAT_DATE => [
-                    'class' => 'kartik\date\DatePicker', // example
-                    'options' => [
-//                        'dateFormat' => 'php:d-M-Y',
-                        'options' => ['class'=>'form-control'],
-                    ]
-                ]
-            ]
-        ],
-    ],
     'components' => [
         'request' => [
             'baseUrl' => '',
@@ -101,12 +46,7 @@ return [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
             'rules' => [
-                '<action:index|login|logout|signup|request-password-reset|reset-password|access-denied|php-info>' => 'site/<action>',
-//              TODO: сделать редирект с /catalog на /catalog/
-//                'catalog'=>'catalog/',
-                'catalog/<categoryAlias:([\/\w\W$]+\/)><productAlias:([^\/]+)$>'=>'catalog/index',
-                'catalog/<categoryAlias:([\/\w\W$]+\/)>/'=>'catalog/index',
-//                'catalog<categoryAlias:[//\w_\/-]+>/'=>'catalog/index',
+//                '<action:index|login|logout|signup|request-password-reset|reset-password>' => 'site/<action>',
             ],
         ],
     ],
