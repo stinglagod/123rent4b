0b302702eb9660009ce7264f3489b407dcc793b6
Алексей Яковлев(dell.home) <busenov@ya.ru>
2020-01-21 09:58:42 +0300

    1

--- backend/controllers/TestController.php
+++ backend/controllers/TestController.php
@@ -25,28 +25,29 @@ class TestController extends Controller
 
     public function actionIndex()
     {
-        $orders=Order::find()->all();
-        foreach ($orders as $order) {
-            echo $order->id;
-            echo '|';
-            echo $order->status_id;
-            /** @var OrderProduct $orderProduct */
-            foreach ($order->orderProducts as $orderProduct) {
-                if (empty($orderProduct->status_id)) {
-                    $orderProduct->status_id=Status::SMETA;
-                    $orderProduct->save();
-                }
-                $orderProduct->changeStatus();
-            }
-//            $order->changeStatus();
-            echo '|';
-            echo $order->status_id;
-            echo '<br>';
-        }
-//        $order=Order::findOne(46);
+//        $orders=Order::find()->all();
+//        foreach ($orders as $order) {
+//            echo $order->id;
+//            echo '|';
+//            echo $order->status_id;
+//            /** @var OrderProduct $orderProduct */
+//            foreach ($order->orderProducts as $orderProduct) {
+//                if (empty($orderProduct->status_id)) {
+//                    $orderProduct->status_id=Status::SMETA;
+//                    $orderProduct->save();
+//                }
+//                $orderProduct->changeStatus();
+//            }
+////            $order->changeStatus();
+//            echo '|';
+//            echo $order->status_id;
+//            echo '<br>';
+//        }
+
+        $order=Order::findOne(34);
 
 //        return $order->status->name;//        return $order->canChangeStatus(Status::CLOSE);
-////        return $order->canChangeStatus(Status::CLOSE)?'':'disabled';
+        return $order->canChangeStatus(Status::CLOSE)?'':'disabled';
 ////        return $order->status->name;
 //        foreach ($order->orderProducts as $orderProduct) {
 //            $orderProduct->changeStatus();
