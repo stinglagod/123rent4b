8df2dca9ba64a26deb267e024ea40e08690a7d9d
Алексей Яковлев(dell.home) <busenov@ya.ru>
2020-03-07 18:54:31 +0300

    Добавил перенос событий с текущих заказов 8

--- console/controllers/OrderController.php
+++ console/controllers/OrderController.php
@@ -11,12 +11,13 @@ class OrderController extends Controller
      * Создание(обновление) событий в google calendar. Начиная с какого года
      * @param int $year
      */
-    public function actionToGCalendar($year=2020)
+    public function actionToGCalendar($year=2020,$all=true)
     {
-        $orders=Order::find()
-            ->where(['>','dateBegin',$year.'-01-01 00:00:00' ])
-            ->andWhere(['is',['googleEvent_id'=>null]])
-            ->orderBy(['id'])->all();
+        $orders=Order::find()->where(['>','dateBegin',$year.'-01-01 00:00:00' ]);
+        if ($all===false) {
+            $orders->andWhere(['is','googleEvent_id',new \yii\db\Expression('null')]);
+        }
+        $orders->orderBy(['id'])->all();
         /** @var Order $order */
         foreach ($orders as $order) {
             echo $order->id;
