8b221491a54fa4665990393a5196e95330675bf5
Алексей Яковлев(dell.home) <busenov@ya.ru>
2020-03-07 18:45:36 +0300

    Добавил перенос событий с текущих заказов 6

--- console/controllers/OrderController.php
+++ console/controllers/OrderController.php
@@ -13,9 +13,11 @@ class OrderController extends Controller
      */
     public function actionToGCalendar($year=2020)
     {
-        $orders=Order::find()->where(['>','dateBegin',$year.'-01-01 00:00:00' ])->all();
+        $orders=Order::find()->where(['>','dateBegin',$year.'-01-01 00:00:00' ])->orderBy(['id'])->all();
         /** @var Order $order */
         foreach ($orders as $order) {
+            echo $order->id;
+            echo ' ';
             echo $order->dateBegin;
             echo $order->changeGoogleCalendar()?' ok':' error';
             echo "\n";
