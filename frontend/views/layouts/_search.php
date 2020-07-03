b3cb9bedef2ec9cca97bd931a6aa1b6cd32a6ee6
Алексей Яковлев(dell.home) <busenov@ya.ru>
2020-07-03 11:47:49 +0300

    Частично реализован поиск

--- frontend/views/layouts/_search.php
+++ frontend/views/layouts/_search.php
@@ -1,13 +1,16 @@
+<?php
+use yii\helpers\Html;
+?>
 <!-- Start Search Popap -->
 <div class="search__area">
     <div class="container" >
         <div class="row" >
             <div class="col-md-12" >
                 <div class="search__inner">
-                    <form action="#" method="get">
+                    <?= Html::beginForm(['/shop/catalog/search'], 'get') ?>
                         <input placeholder="Поиск здесь... " type="text">
                         <button type="submit"></button>
-                    </form>
+                    <?= Html::endForm() ?>
                     <div class="search__close__btn">
                         <span class="search__close__btn_icon"><i class="zmdi zmdi-close"></i></span>
                     </div>
---
+++ rent/services/search/ProductIndexer.php
@@ -0,0 +1,142 @@
+<?php
+
+namespace rent\services\search;
+
+use Elasticsearch\Client;
+use rent\entities\Shop\Category;
+use rent\entities\Shop\Product\Product;
+use rent\entities\Shop\Product\Value;
+use yii\helpers\ArrayHelper;
+use Elasticsearch\Common\Exceptions\Missing404Exception;
+
+class ProductIndexer
+{
+    private $client;
+
+    public function __construct(Client $client)
+    {
+        $this->client = $client;
+    }
+
+    public function clear(): void
+    {
+        try {
+            $this->client->search(['index'=>'shop']);
+        } catch (Missing404Exception $e) {
+            $this->createIndex();
+        }
+
+        $this->client->deleteByQuery([
+            'index' => 'shop',
+            'type' => 'products',
+            'body' => [
+                'query' => [
+                    'match_all' => new \stdClass(),
+                ],
+            ],
+        ]);
+    }
+
+    public function index(Product $product): void
+    {
+        $this->client->index([
+            'index' => 'shop',
+            'type' => 'products',
+            'id' => $product->id,
+            'body' => [
+                'id' => $product->id,
+                'name' => $product->name,
+                'description' => strip_tags($product->description),
+                'priceSale' => $product->priceSale_new,
+                'priceRent' => $product->priceRent_new,
+                'rating' => $product->rating,
+                'brand' => $product->brand_id,
+                'categories' => ArrayHelper::merge(
+                    [$product->category->id],
+                    ArrayHelper::getColumn($product->category->parents, 'id'),
+                    ArrayHelper::getColumn($product->categories, 'id'),
+                    array_reduce(array_map(function (Category $category) {
+                        return ArrayHelper::getColumn($category->parents, 'id');
+                    }, $product->categories), 'array_merge', [])
+                ),
+                'tags' => ArrayHelper::getColumn($product->tagAssignments, 'tag_id'),
+                'values' => array_map(function (Value $value) {
+                    return [
+                        'characteristic' => $value->characteristic_id,
+                        'value_string' => (string)$value->value,
+                        'value_int' => (int)$value->value,
+                    ];
+                }, $product->values),
+            ],
+        ]);
+    }
+
+    public function remove(Product $product): void
+    {
+        $this->client->delete([
+            'index' => 'shop',
+            'type' => 'products',
+            'id' => $product->id,
+        ]);
+    }
+
+    public function createIndex():void
+    {
+        $this->client->indices()->create([
+            'index' => 'shop',
+            'body' => [
+                'mappings' => [
+                    'products' => [
+                        '_source' => [
+                            'enabled' => true,
+                        ],
+                        'properties' => [
+                            'id' => [
+                                'type' => 'integer',
+                            ],
+                            'name' => [
+                                'type' => 'text',
+                            ],
+                            'description' => [
+                                'type' => 'text',
+                            ],
+                            'price' => [
+                                'type' => 'integer',
+                            ],
+                            'rating' => [
+                                'type' => 'float',
+                            ],
+                            'brand' => [
+                                'type' => 'integer',
+                            ],
+                            'categories' => [
+                                'type' => 'integer',
+                            ],
+                            'tags' => [
+                                'type' => 'integer',
+                            ],
+                            'values' => [
+                                'type' => 'nested',
+                                'properties' => [
+                                    'characteristic' => [
+                                        'type' => 'integer'
+                                    ],
+                                    'value_string' => [
+                                        'type' => 'keyword',
+                                    ],
+                                    'value_int' => [
+                                        'type' => 'integer',
+                                    ],
+                                ]
+                            ]
+                        ]
+                    ]
+                ]
+            ]
+        ]);
+    }
+    public function deleteIndex(): void
+    {
+        $this->client->indices()->delete(['index' => 'shop']);
+    }
+}
