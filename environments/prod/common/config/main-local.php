09b835a46939b16f9d6bd033374fe795656e58b5
Алексей Яковлев(dell.home) <busenov@ya.ru>
2020-08-31 06:55:25 +0300

    Поправил envirnments

--- environments/prod/common/config/main-local.php
+++ environments/prod/common/config/main-local.php
@@ -8,14 +8,6 @@ return [
             'password' => '',
             'charset' => 'utf8',
         ],
-        'mailer' => [
-            'class' => 'yii\swiftmailer\Mailer',
-            'viewPath' => '@common/mail',
-            // send all mails to a file by default. You have to set
-            // 'useFileTransport' to false and configure a transport
-            // for the mailer to send real emails.
-            'useFileTransport' => true,
-        ],
         'authClientCollection' => [
             'class' => 'yii\authclient\Collection',
             'clients' => [
