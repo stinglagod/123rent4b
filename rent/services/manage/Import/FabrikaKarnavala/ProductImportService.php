af4dca89ddb40474a0caf58f9d8c144217329d61
Алексей Яковлев(dell.home) <busenov@ya.ru>
2020-09-22 22:25:53 +0300

    запретили редактировать категории

--- rent/services/manage/Import/FabrikaKarnavala/ProductImportService.php
+++ rent/services/manage/Import/FabrikaKarnavala/ProductImportService.php
@@ -238,6 +238,7 @@ class ProductImportService
      */
     private function updCategory ($file)
     {
+        return true;
         $item = array();
         $this->setLog('Запуск: updCategory', false);
 
