6e0197e63ad00045575c4906f360d2af6f92cbc4
Алексей Яковлев(dell.home) <busenov@ya.ru>
2021-10-25 13:27:13 +0300

    Исправлена ошибка при сбросе пароля 7

--- frontend/controllers/auth/AuthController.php
+++ frontend/controllers/auth/AuthController.php
@@ -22,21 +22,25 @@ class AuthController extends Controller
      */
     public function actionLogin()
     {
-
         if (!Yii::$app->user->isGuest) {
             return $this->goHome();
         }
 
         $form = new LoginForm();
         $signupForm= new SignupForm();
-        dump($form->load(Yii::$app->request->post()));
-        dump( $form->validate());
+
+//        dump($form->load(Yii::$app->request->post()));
+//        dump( $form->validate());
+
         if ($form->load(Yii::$app->request->post()) && $form->validate()) {
             try {
                 $user = $this->service->auth($form);
                 Yii::$app->user->login($user, $form->rememberMe ? 3600 * 24 * 30 : 0);
                 Yii::$app->session->setFlash('success', 'Вы успешно вошли');
-                return $this->goHome();
+                dump($form->load(Yii::$app->request->post()));
+                dump( $form->validate());
+
+//                return $this->goHome();
             } catch (\DomainException $e) {
                 Yii::$app->errorHandler->logException($e);
                 Yii::$app->session->setFlash('error', $e->getMessage());
