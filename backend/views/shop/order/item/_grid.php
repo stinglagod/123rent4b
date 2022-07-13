1fac15e1a03c80d311911e548cccf86d4a1cc5e1
Алексей Яковлев(dell.home) <busenov@ya.ru>
2022-07-13 09:43:04 +0300

    Сортировка позиции в заказе 3

--- backend/views/shop/order/item/_grid.php
+++ backend/views/shop/order/item/_grid.php
@@ -250,8 +250,8 @@ use rent\helpers\OrderHelper;
                         ]);
                 },
                 'sort' => function ($url, OrderItem $model, $key)  {
-                    if (!$model->readOnly() and (!$model->children)) {
-                        $result='';
+                    $result='';
+                    if (!$model->readOnly() ) {
                         if ($model->sort != 0) {
                             $result.= '<button class="btn btn-default ' . ($model->order->readOnly()?'disabled':'move-block') . '" data-url="'.Url::toRoute(['item-move-up-ajax','id'=>$model->order->id,'item_id'=>$model->id]).'" data-method="POST" type="button"><span class="glyphicon glyphicon-arrow-up" aria-hidden="true"></span></button>';
                         }
@@ -259,8 +259,8 @@ use rent\helpers\OrderHelper;
                         if (($model->sort+1)!=$model->block->getCountChildren()) {
                             $result.= '<button class="btn btn-default ' . ($model->order->readOnly()?'disabled':'move-block') . '" data-url="'.Url::toRoute(['item-move-down-ajax','id'=>$model->order->id,'item_id'=>$model->id]).'" data-method="POST" type="button"><span class="glyphicon glyphicon-arrow-down" aria-hidden="true"></span></button>';
                         }
-                        return $result;
                     }
+                    return $result;
 //                        return
 //                            '<button class="btn btn-default ' . ($model->order->readOnly()?'disabled':'move-block') . '" data-url="'.Url::toRoute(['item-move-up-ajax','id'=>$model->order->id,'item_id'=>$model->id]).'" data-method="POST" type="button"><span class="glyphicon glyphicon-arrow-up" aria-hidden="true"></span></button>'
 //                            .
