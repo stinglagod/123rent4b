00736631212d2138bdfb9d2aaf8a1fe742390d25
stinglagod <124559623+stinglagod@users.noreply.github.com>
2023-10-11 14:30:29 +0300

    Create Sketch.php

---
+++ Sketch.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace app\models;
+
+use yii\db\ActiveRecord;
+use yii\web\UploadedFile;
+use yiidreamteam\upload\FileUploadBehavior;
+
+class Sketch extends ActiveRecord
+{
+    /**
+     * @var UploadedFile
+     */
+    public $file;
+
+    public static function tableName()
+    {
+        return 'order_sketch_files';
+    }
+
+    public function behaviors()
+    {
+        return [
+            [
+                'class' => FileUploadBehavior::class,
+                'attribute' => 'file',
+                'filePath' => '@staticRoot/order/sketches/[[id]].[[extension]]',
+                'fileUrl' => '@static/order/sketches/[[id]].[[extension]]',
+            ],
+        ];
+    }
+
+    public function rules()
+    {
+        return [
+            [['file'], 'file'],
+        ];
+    }
+}
